61433c71c091121a10134b589188fe5e64aecba3
Zach Albia <zach.albia@gmail.com>
2016-12-01 21:57:19 +0800

    added demo showcasing use of .NET Data Provider for Oracle

--- kent-demo/Default.aspx.cs
+++ kent-demo/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OracleClient;
 
 namespace kent_demo
 {
@@ -11,7 +12,12 @@ namespace kent_demo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var con = new OracleConnection("Data Source=localhost;Persist Security Info=True;User ID=demo;Password=1234;Unicode=True");
+            var commandText = "INSERT INTO WORLD VALUES ('Philippines', 'Manila', 102834982)";
+            con.Open();
+            var command = new OracleCommand(commandText, con);
+            command.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
